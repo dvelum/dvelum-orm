38ab2dfad8d08367fb85dfb8a0a3d24a44d9f612
Kirill Yegorov <kirill.a.egorov@gmail.com>
2021-08-11 15:10:54 +0300

    refactoring controllers

---
+++ src/Dvelum/Orm.php
@@ -0,0 +1,24 @@
+<?php
+namespace Dvelum;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class Orm factory
+ * Backward compatibility
+ * @package Dvelum
+ * @deprecated
+ */
+class Orm
+{
+    static private ContainerInterface $di;
+
+    public static function setContainer(ContainerInterface $di) : void
+    {
+        self::$di = $di;
+    }
+
+    public static function factory() : \Dvelum\Orm\Orm
+    {
+        return self::$di->get(\Dvelum\Orm\Orm::class);
+    }
+}
